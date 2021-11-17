721e29cb655cd94dcac27cf2d24c53bfcefb9ab7
SpicyIRamen <spicyiramen@gmail.com>
2021-11-17 22:17:42 +0100

    CRUD, Exceptions for all calls, can query by surname, all error handling in JSON format

--- src/main/java/se/iths/rest/StudentRest.java
+++ src/main/java/se/iths/rest/StudentRest.java
@@ -1,7 +1,124 @@
 package se.iths.rest;
 
+import se.iths.entity.Student;
+import se.iths.service.StudentService;
 
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+@Path("student")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class StudentRest {
 
+    @Inject
+    StudentService studentService;
+
+    @Path("new")
+    @POST
+    public Response createStudent(Student student)
+    {
+        try {
+            studentService.createStudent(student);
+            return Response.ok(student).build();
+        } catch (WebApplicationException i){
+            return Response.status(Response.Status.BAD_REQUEST).type(MediaType.APPLICATION_JSON).build();
+        }
+    }
+
+    @Path("update/{id}")
+    @PATCH
+    public Response updateStudent(@PathParam("id") Long id, Student student) {
+
+        Student updatedStudent = studentService.findStudentById(id);
+
+        String errorMessage = "{\"Error\": \"No students found with id " + id + "\"}";
+        try {
+
+            if (updatedStudent == null) {
+                return Response.status(Response.Status.NOT_FOUND)
+                        .entity(errorMessage).type(MediaType.APPLICATION_JSON).build();
+            }
+
+            updatedStudent.setFirstName(student.getName());
+            updatedStudent.setLastName(student.getLastName());
+            updatedStudent.setEmail(student.getEmail());
+            updatedStudent.setPhoneNumber(student.getPhoneNumber());
+
+            studentService.updateStudent(updatedStudent);
+
+            return Response.accepted(updatedStudent).build();
+        } catch (WebApplicationException i) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity(errorMessage).type(MediaType.APPLICATION_JSON).build();
+        }
+
+    }
+
+    @Path("{id}")
+    @GET
+    public Response getStudent(@PathParam("id") Long id) {
+        Student student = studentService.findStudentById(id);
+        try {
+            if (student == null) {
+                String errorMessage = "{\"Error\": \"No students found with that id.\"}";
+                return Response.status(Response.Status.NOT_FOUND)
+                        .entity(errorMessage).type(MediaType.APPLICATION_JSON).build();
+            }
+
+            return Response.accepted(student).build();
+        } catch (WebApplicationException i) {
+            String errorMessage = "{\"Error\": \"No students found with that id.\"}";
+            return Response.status(Response.Status.BAD_REQUEST).entity(errorMessage).type(MediaType.APPLICATION_JSON).build();
+        }
+
+    }
+
+    @Path("getall")
+    @GET
+    public List<Student> getAllStudent() {
+        List<Student> foundAllStudents = studentService.getAllStudents();
+        if (foundAllStudents == null || foundAllStudents.isEmpty()) {
+            String errorMessage = "{\"Error\": \"No students found.\"}";
+            throw new WebApplicationException(Response.status(Response.Status.NOT_FOUND)
+                    .entity(errorMessage).type(MediaType.APPLICATION_JSON).build());
+        }
+
+        return studentService.getAllStudents();
+    }
+
+    @Path("query")
+    @GET
+    public Response getStudentByLastName(@QueryParam("lastname") String lastname)
+    {
+        String message = "{\"Error\": \"No student found with that lastname.\"}";
+            List<Student> foundStudent = studentService.getStudentByLastName(lastname);
+            if (foundStudent == null || foundStudent.isEmpty()) {
+                throw new WebApplicationException(Response.status(Response.Status.BAD_REQUEST)
+                        .entity(message).type(MediaType.APPLICATION_JSON).build());
+            }
+            return Response.ok(foundStudent).build();
+    }
+
+    @Path("delete/{id}")
+    @DELETE
+    public Response deleteStudent(@PathParam("id")Long id)
+    {
+        Student getStudent = studentService.findStudentById(id);
+        String errorMessage = "{\"Error\": \"Student with id " + id + " was not found\"}";
+        if (getStudent == null)
+        {
+            throw new WebApplicationException(Response.status(Response.Status.NOT_FOUND)
+                    .entity(errorMessage).type(MediaType.APPLICATION_JSON).build());
+        }
+
+        studentService.deleteStudent(getStudent);
+        String message = "{\"Notice\": \"Student with id " + id + " has been deleted.\"}";
+        return Response.status(Response.Status.ACCEPTED)
+                .entity(message).type(MediaType.APPLICATION_JSON).build();
+    }
 
 }
--- src/main/java/se/iths/service/StudentService.java
+++ src/main/java/se/iths/service/StudentService.java
@@ -1,7 +1,53 @@
 package se.iths.service;
 
 
+import se.iths.entity.Student;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Transactional
 public class StudentService {
 
+    @PersistenceContext
+    EntityManager entityManager;
 
+    public Student createStudent(Student student)
+    {
+        entityManager.persist(student);
+        return student;
+    }
+
+    public void updateStudent(Student student)
+    {
+        entityManager.merge(student);
+    }
+
+    public Student findStudentById(Long id)
+    {
+        return entityManager.find(Student.class, id);
+    }
+
+    public List<Student> getStudentByLastName(String lastName)
+    {
+        return entityManager.createQuery("SELECT i FROM Student i WHERE i.lastName LIKE :lastName")
+                .setParameter("lastName", lastName)
+                .setMaxResults(3).getResultList();
+    }
+
+    public List<Student> getAllStudents()
+    {
+        return entityManager.createQuery("SELECT i from Student i", Student.class).getResultList();
+    }
+
+    public void deleteStudent(Student student)
+    {
+        if (!entityManager.contains(student))
+        {
+            student = entityManager.merge(student);
+        }
+        entityManager.remove(student);
+    }
 }
